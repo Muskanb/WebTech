26bf96a0afb3487b77e7bafaee3eaabe98f33143
devt-10 <123680232+devt-10@users.noreply.github.com>
2023-09-06 10:42:08 +0530

    Add files via upload

---
+++ week5/boxes.js
@@ -0,0 +1,9 @@
+function alertme(){
+    alert('Warning your computer is gonna explode!')
+}
+function promptme(){
+    prompt('Do you want to die?')
+}
+function confirmme(){
+    confirm('How is life?')
+}
