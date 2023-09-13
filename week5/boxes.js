0942bd4e1be601955c5aa4ddc8a7b6e21db8f26a
devt-10 <123680232+devt-10@users.noreply.github.com>
2023-09-13 11:14:43 +0530

    Delete week5 directory

--- week5/boxes.js
+++
@@ -1,9 +0,0 @@
-function alertme(){
-    alert('Warning your computer is gonna explode!')
-}
-function promptme(){
-    prompt('Do you want to die?')
-}
-function confirmme(){
-    confirm('How is life?')
-}
--- week5/create.js
+++
@@ -1,7 +0,0 @@
-function newwindow(){
-    const window=open();
-    const header=document.createElement("h1");
-    header.innerText="The secret is within you!";
-    window.document.body.appendChild(header);
-    
-}
